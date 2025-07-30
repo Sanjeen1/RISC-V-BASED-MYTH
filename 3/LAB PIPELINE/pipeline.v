fcd6eaed49d7f2eb2442ea09dc3b2ff9bd2b310c
S.Suman <shubhamsanjeen@gmail.com>
2025-07-30 21:32:19 +0530

    one lab complete only

---
+++ 3/LAB PIPELINE/pipeline.v
@@ -0,0 +1,30 @@
+\m5_TLV_version 1d: tl-x.org
+\m5
+   
+   // ============================================
+   // Welcome, new visitors! Try the "Learn" menu.
+   // ============================================
+   
+   //use(m5-1.0)   /// uncomment to use M5 macro library.
+\SV
+   // Macro providing required top-level module definition, random
+   // stimulus support, and Verilator config.
+   m5_makerchip_module   // (Expanded in Nav-TLV pane.)
+\TLV
+   $reset = *reset;
+   |pip
+      @1
+         $err1 = $bad_input || $illegal_up;
+  
+      @3
+         $err2 = $over_flow || $err1 ;
+  
+      @6
+         $err3 = $err2 || $div_by_zero;
+
+   
+   // Assert these to end simulation (before the cycle limit).
+   *passed = *cyc_cnt > 40;
+   *failed = 1'b0;
+\SV
+   endmodule
